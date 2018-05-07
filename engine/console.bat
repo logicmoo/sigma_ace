88d73082a99142cc20ab41672833e816786f6019
Douglas R. Miles <logicmoo@gmail.com>
2018-05-07 04:23:38 -0700

    Sigma ACE: "

---
+++ engine/console.bat
@@ -0,0 +1,8 @@
+:Label1
+
+copy /y sigma_image_blank.P sigma_image.P
+
+start "Sigma Server Compiler" /SHARED /B /MIN /WAIT /LOW /D. "..\swi\bin\plcon.exe" -f sigma_console.P
+
+
+
---
+++ engine/merge_console.bat
@@ -0,0 +1,7 @@
+:Label1
+
+copy /y sigma_image_merge.P sigma_image.P
+start "Sigma Server Compiler" /SHARED /B /MIN /WAIT /LOW /D. "..\swi\bin\plcon.exe" -f sigma_console.P
+
+
+
